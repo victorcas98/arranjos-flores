aeb17efa240d80c9c02097687ffca83e5291f165
victorcas98 <victor.cas98@outlook.com>
2026-05-25 20:17:56 -0300

    removi texto

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/arranjos-flores/',   // 👈 adicione esta linha
+  base: '/arranjos-flores/',
   plugins: [
     react(),
     tailwindcss(),
